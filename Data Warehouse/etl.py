1b7019a63cf1675015d7b7afae3976e55c0f9b4a
Vladyslav Tarasenko <shekspir.well@gmail.com>
2022-12-17 17:31:00 +0200

    add some comments

--- Data Warehouse/etl.py
+++ Data Warehouse/etl.py
@@ -4,18 +4,33 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    Loads S3 files data into staging tables.
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    Loads data into fact and dimension tables.
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    """
+    - Establishes connection with the database and gets cursor to it.
+
+    - Loads S3 files data into staging `events` and `songs` tables.
+
+    - Loads data into fact and dimension tables.
+
+    - Finally, closes the connection.
+    """
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
